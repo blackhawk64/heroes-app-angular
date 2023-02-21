3823670bc857ab809c539a69d1469980cffe4ec7
Alexis Alcantara <alexisv.alcantara@gmail.com>
2023-02-20 18:20:21 -0600

    Creacion de tarjetas de heroe
    Creacion de la vista listado en donde se muestran los heroes con su imagen e informacion breve. El ruteo de prueba hacia ver heroe y editar tambien fue agregado

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,16 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-agregar',
   templateUrl: './agregar.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class AgregarComponent implements OnInit {
-
-  constructor() { }
+  constructor(private _route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this._route.params.subscribe(({ id }) => console.log(id));
   }
-
 }
--- src/app/heroes/pages/listado/listado.component.ts
+++ src/app/heroes/pages/listado/listado.component.ts
@@ -1,19 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService } from '../../services/heroes.service';
+import { Heroe } from '../interfaces/heroes.interface';
 
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
-  styles: [
-  ]
+  styles: []
 })
 export class ListadoComponent implements OnInit {
+
+  heroes: Heroe[] = [];
 
   constructor(private heroesService: HeroesService) { }
 
   ngOnInit(): void {
     this.heroesService.getHeroes()
-        .subscribe(response => console.log(response));
+      .subscribe(heroes => this.heroes = heroes);
   }
 
 }
